fb634cad8299cf85285ebff7d44d711ddc6193ea
Max Maass <max@velcommuta.de>
2015-11-20 11:36:00 +0100

    Add slot and increment calc, tests
    Add the calculation functions for slots and increment values.
    Added test cases with known-good values from the python
    implementation.

--- src/main/java/de/velcommuta/libvicbf/VICBF.java
+++ src/main/java/de/velcommuta/libvicbf/VICBF.java
@@ -1,9 +1,12 @@
 package de.velcommuta.libvicbf;
 
+import java.io.UnsupportedEncodingException;
 import java.nio.ByteBuffer;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.util.Hashtable;
+import java.math.BigInteger;
+import java.math.BigDecimal;
 
 /**
  * Implementation of a variable-increment counting bloom filter, as proposed by Rottenstreich et al.
@@ -22,11 +25,11 @@ import java.util.Hashtable;
  * implementation, you'll have to build your own. :)
  */
 public class VICBF {
-    private int mSlots;
-    private int mExpectedEntries;
+    private BigInteger mSlotsBI;
     private int mHashFunctions;
     private Hashtable<Short, Byte> mBloomFilter;
     private int L = 4;
+    private BigInteger LBI = BigInteger.valueOf(4);
 
     /**
      * Constructor for new VICBF
@@ -34,16 +37,13 @@ public class VICBF {
      * @param expected_entries Expected number of entries
      * @param hash_functions Number of hash functions to use
      */
-    public VICBF(int slots, int expected_entries, int hash_functions) {
+    public VICBF(int slots, int hash_functions) {
         if (slots < 1) {
             throw new IllegalArgumentException("slots must be at least 1");
-        } else if (expected_entries < 1) {
-            throw new IllegalArgumentException("Expected Entries must be at least 1");
         } else if (hash_functions < 1) {
             throw new IllegalArgumentException("Hash functions must be at least 1");
         }
-        mSlots = slots;
-        mExpectedEntries = expected_entries;
+        mSlotsBI = BigInteger.valueOf(slots);
         mHashFunctions = hash_functions;
         mBloomFilter = new Hashtable<>();
     }
@@ -67,14 +67,24 @@ public class VICBF {
     protected Short calculateSlot(String key, int i) {
         try {
             MessageDigest md = MessageDigest.getInstance("SHA-1");
-            byte[] digest = md.digest((key + i).getBytes());
-            return (short) (ByteBuffer.wrap(digest).getDouble() % mSlots);
-        } catch (NoSuchAlgorithmException e) {
+            byte[] digest = md.digest((key + i).getBytes("utf8"));
+            // BigInteger interprets the input as signed, but we want unsigned,
+            // as the python counterpart uses unsigned numbers. So, we need to
+            // make sure that the number is always interpreted as positive. We
+            // can achieve this by prepending a 0x00 to the hex value, thereby
+            // nulling the sign.
+            byte[] dst = new byte[digest.length + 1];
+            System.arraycopy(new byte[] {0x00}, 0, dst, 0, 1);
+            System.arraycopy(digest, 0, dst, 1, digest.length);
+            // Interpret as a number and get the result modulo the number of
+            // slots to determine the slot.
+            return new BigInteger(dst).mod(mSlotsBI).shortValue();
+        } catch (NoSuchAlgorithmException | UnsupportedEncodingException e) {
             e.printStackTrace();
             return null;
         }
     }
-
+    
 
     /**
      * Calculate the increment value, based on key and hash function
@@ -85,11 +95,24 @@ public class VICBF {
     protected Byte calculateIncrement(String key, int i) {
         try {
             MessageDigest md = MessageDigest.getInstance("SHA-1");
-            byte[] digest = md.digest((-i + key).getBytes());
-            return (byte) (ByteBuffer.wrap(digest).getDouble() % L);
-        } catch (NoSuchAlgorithmException e) {
+            byte[] digest = md.digest((-i + key).getBytes("utf8"));
+            byte rv = new BigInteger(digest).mod(LBI).byteValue();
+            return (byte) (rv + L);
+        } catch (NoSuchAlgorithmException | UnsupportedEncodingException e) {
             e.printStackTrace();
             return null;
         }
+    }
+    
+    // TODO Debugging helper, remove
+    final protected static char[] hexArray = "0123456789ABCDEF".toCharArray();
+    public static String bytesToHex(byte[] bytes) {
+        char[] hexChars = new char[bytes.length * 2];
+        for ( int j = 0; j < bytes.length; j++ ) {
+            int v = bytes[j] & 0xFF;
+            hexChars[j * 2] = hexArray[v >>> 4];
+            hexChars[j * 2 + 1] = hexArray[v & 0x0F];
+        }
+        return new String(hexChars);
     }
 }
---
+++ src/main/test/de/velcommuta/libvicbf/VICBFTest.java
@@ -0,0 +1,31 @@
+package de.velcommuta.libvicbf;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class VICBFTest {
+
+	@Test
+	public void test_slot_calculation() {
+		VICBF test = new VICBF(10000, 3);
+		short v = test.calculateSlot("123", 0);
+		assertEquals(v, (short) 9653);
+		v = test.calculateSlot("testvicbf", 0);
+		assertEquals(v, (short) 8525);
+		v = test.calculateSlot("decafbad", 0);
+		assertEquals(v, (short) 7634);
+	}
+	
+	@Test
+	public void test_increment_calculation() {
+		VICBF test = new VICBF(10000, 3);
+		byte t = test.calculateIncrement("123", 0);
+		assertEquals(t, (byte) 5);
+		t = test.calculateIncrement("testvicbf", 0);
+		assertEquals(t, (byte) 7);
+		t = test.calculateIncrement("decafbad", 0);
+		assertEquals(t, (byte) 5);
+	}
+
+}
